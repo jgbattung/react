60ddd2312976de84ff2ee24d3a60dcd2439c6d2d
Jireh Battung <jirehgbattung@gmail.com>
2022-06-22 18:09:26 +0800

    uploaded edited react-practice folder

---
+++ react-practice-hw/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App () {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<h1>LEARNING REACT!</h1>
+				<a className="App-link" href="https://github.com/jgbattung/" target="_blank" rel="noopener noreferrer">
+					Checkout my github page!
+				</a>
+			</header>
+		</div>
+	);
+}
+
+export default App;
